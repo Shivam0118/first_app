637ceacdb5b0c108f66da57a1762d8b7a56a6784
Shivam <shivam8728949475@gmail.com>
2023-01-25 10:38:46 +0530

    toggle created

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,124 @@
-//import 'dart:html';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Screen1(), routes: {
-    "/": (context) => Screen1(),
-    "/Screen2": (context) => Screen2(),
-    "/Screen3": (context) => Screen3(),
-  }));
+  runApp(MyHome());
 }
 
-class Screen1 extends StatelessWidget {
-  const Screen1({super.key});
+class MyHome extends StatefulWidget {
+  const MyHome({super.key});
 
   @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  bool myThemeDark = false;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.yellow,
-        body: SafeArea(
-          child: Center(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                InkWell(
-                    onTap: () {
-                      print("On Screen 1");
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: ((context) => Screen2() as Widget)));
-                    },
-                    child: Container(
-                      child: Text("button 1"),
-                    ))
-              ])),
-        ));
+    return MaterialApp(
+      theme: (myThemeDark) ? ThemeData.dark() : ThemeData(),
+      home: Screen_1(
+        sourceButton: 1,
+        changeThemeToDark: changeMyAppTheme,
+      ),
+    );
+  }
+
+  void changeMyAppTheme(bool changeToBlack) {
+    setState(() {
+      myThemeDark = changeToBlack;
+    });
   }
 }
 
-class Screen2 extends StatelessWidget {
-  const Screen2({super.key});
+class Screen_2 extends StatefulWidget {
+  final Color color;
+  final Function(bool) changeThemeToDark;
+  const Screen_2(
+      {super.key, required this.color, required this.changeThemeToDark});
+
+  @override
+  State<Screen_2> createState() => _Screen_2State();
+}
+
+class _Screen_2State extends State<Screen_2> {
+  Color? myColor;
+  bool myValue = false;
+
+  @override
+  void initState() {
+    myColor = widget.color;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.blue,
         body: SafeArea(
             child: Center(
-          child: InkWell(
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: ((context) => Screen3())));
-            },
-            child: Container(
-              child: Text("button 2"),
+                child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Switch(
+                value: myValue,
+                onChanged: (value) {
+                  setState(() {
+                    myValue = value;
+                  });
+                  widget.changeThemeToDark(value);
+                }),
+            InkWell(
+              onTap: () {
+                print("On Screen 1");
+                Navigator.pop(context);
+              },
+              child: Container(
+                child: Text("Go to screen 1"),
+              ),
             ),
-          ),
-        )));
+          ],
+        ),
+      ],
+    ))));
+  }
+
+  void changeBackground(Color color) {
+    myColor = color;
+    print(color);
+    print("Reached here");
+    setState(() {});
+  }
+}
+
+class Screen_1 extends StatelessWidget {
+  final int sourceButton;
+
+  final Function(bool) changeThemeToDark;
+
+  const Screen_1(
+      {super.key, required this.sourceButton, required this.changeThemeToDark});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+            child: Center(
+                child: InkWell(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: ((context) => Screen_2(
+                    color: Colors.green,
+                    changeThemeToDark: changeThemeToDark))));
+      },
+      child: Container(
+        child: Text("Go to screen 2"),
+      ),
+    ))));
   }
 }
 
@@ -65,17 +128,16 @@ class Screen3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.blue,
         body: SafeArea(
             child: Center(
-          child: InkWell(
-            onTap: () {
-              Navigator.pushNamed(context, "/");
-            },
-            child: Container(
-              child: Text("button 3"),
-            ),
+                child: InkWell(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Container(
+            child: Text("Screen 3"),
           ),
-        )));
+        ))));
   }
 }
